f6b47b5a7fd6da4d5e2f3082d6a7ecd69feb0cb4
rushabhtolia99 <72116591+rushabhtolia99@users.noreply.github.com>
2021-03-18 21:06:01 -0500

    Add files via upload

---
+++ boat.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class boat : MonoBehaviour {
+
+	public float turnSpeed = 1000f;
+	public float accellerateSpeed = 1000f;
+
+	private Rigidbody rbody;
+
+	// Use this for initialization
+	void Start () 
+	{
+		rbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis("Vertical");
+
+		rbody.AddTorque(0f,h*turnSpeed*Time.deltaTime,0f);
+		rbody.AddForce(transform.forward*v*accellerateSpeed*Time.deltaTime);
+
+
+	}
+}
---
+++ switchMode.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class switchMode : MonoBehaviour {
+
+	public GameObject boat;
+	public GameObject boatCamera;
+	public GameObject player;
+	public GameObject playerStartPos;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//set to boat mode//
+		if(Input.GetKey("1"))
+		{
+			boat.GetComponent<Rigidbody>().isKinematic = false;
+			boat.GetComponent<boat>().enabled = true;
+			boatCamera.SetActive(true);
+
+			player.SetActive(false);
+		}
+
+		//set to FPS mode//
+		if(Input.GetKey("2"))
+		{
+			boat.GetComponent<Rigidbody>().isKinematic = true;
+			boat.GetComponent<boat>().enabled = false;
+			boatCamera.SetActive(false);
+
+			player.SetActive(true);
+			player.transform.position = playerStartPos.transform.position;
+		}
+	}
+}
